c2dab21ca9b48e52c7bf637a849ca0564fd68720
Carlos <carlos42044@gmail.com>
2017-08-10 01:19:59 -0400

    Added more image processing

--- js/main.js
+++ js/main.js
@@ -1,21 +1,19 @@
 
 
 var originalImage;
-var canvasFocus = false;
 var inputCreated = false;
 var input = document.createElement("input");
 var textEditID = 1000;
 
-var text
 
 function dragNdrop() {
-	container = document.querySelector("div");
+	buttons();
 
 	var canvas = document.getElementById("dropzone"),
 		context = canvas.getContext("2d"),
 		img = document.createElement("img"),
-		// mouseDown = false,
-		// brushColor = "rgb(0, 0, 0)",
+		mouseDown = false,
+	    brushColor = "rgb(0, 0, 0)",
 		hasText = true,
 		clearCanvas = function () {
 			if (hasText) {
@@ -54,56 +52,51 @@ function dragNdrop() {
 
 		// save the original image
 		originalImage = canvas.toDataURL("image/png");
-		//window.open(canvas.toDataURL("image/png"));
+
 	}, false);
 
 	// Mousedown event listener
 	canvas.addEventListener("mousedown", function (evt) {
-		canvasFocus	= true;
 		clearCanvas();
+		mouseDown = true;
+		context.beginPath();
 
-		var xPos = evt.clientX - canvas.getBoundingClientRect().left;
-		var yPos = evt.clientY - canvas.getBoundingClientRect().top;
-		// mouseDown = true;
-		// context.beginPath();
-		drawNote(evt);
-		addTextBox();
-		//grayScale();
-		darkenImage(-10, -10, -10);
-		//invertColors();
-		//brightenImage();
+		drawNote(evt);	
 	}, false);
 
-	// Enter key event listener, if canvas has focus(have not implemented yet)
 	// this is where the input box would get saved and sent to the database
 	window.addEventListener("keyup", function (evt) {
 		if ( 13 == evt.keyCode && canvasFocus) {
-			//alert("you press enter!");
 			console.log("you press enter!");
-		  alert(input.value());
 		  //document.getElementById('input').style.display="none";
-			context.drawImage(img, 0, 0, 500, 500);
-			context.clearRect(0, 0, canvas.width, canvas.height);
+			//context.drawImage(img, 0, 0, 500, 500);
+			//context.clearRect(0, 0, canvas.width, canvas.height);
 
 		}
 	});
 
 	// Mpuseup event listener
 	canvas.addEventListener("mouseup", function (evt) {
-		// mouseDown = false;
-		// var colors = context.getImageData(evt.layerX, evt.layerY, 1, 1).data;
-		// brushColor = "rgb(" + colors[0] + ", " + colors[1] + ", " + colors[2] + ")";
+		mouseDown = false;
+		var colors = context.getImageData(evt.layerX, evt.layerY, 1, 1).data;
+		brushColor = "rgb(" + colors[0] + ", " + colors[1] + ", " + colors[2] + ")";
 	}, false);
 
 	// Mousemove event listener
 	canvas.addEventListener("mousemove", function (evt) {
-		// if (mouseDown) {
-		// 	context.strokeStyle = brushColor;
-		// 	context.lineWidth = 20;
-		// 	context.lineJoin = "round";
-		// 	context.lineTo(evt.layerX+1, evt.layerY+1);
-		// 	context.stroke();
-		// }
+
+		 var xPos = evt.clientX - canvas.getBoundingClientRect().left;
+		 var yPos = evt.clientY - canvas.getBoundingClientRect().top;
+		if (mouseDown) {
+			// works like an eyedropper tool 
+			//context.strokeStyle = brushColor;
+			context.strokeStyle =  "#ffff00";
+			context.lineWidth = 10;
+			context.lineJoin = "round";
+			//context.lineTo(evt.layerX+1, evt.layerY+1);
+			context.lineTo(xPos, yPos);
+			context.stroke();
+		}
 	}, false);
 
 	// dragover event listener
@@ -129,30 +122,118 @@ function dragNdrop() {
 		evt.preventDefault();
 	}, false);
 
-	// Save image button
+	
+}
+
+function buttons() {
+	var canvas = document.getElementById("dropzone");
+	var ctx  = canvas.getContext("2d");
+	// Load original image
+	var loadOriginal = document.createElement("button");
+	loadOriginal.innerHTML = "Reset image";
+	var img = new Image;
+
+	loadOriginal.addEventListener("click", function(evt) {
+		ctx.drawImage(img, 0, 0);
+			img.src = originalImage;
+
+	})
+	document.getElementById("button-div").appendChild(loadOriginal);
+
+	// ################################################################################
+	// Save edited image button
 	var saveImage = document.createElement("button");
 	saveImage.innerHTML = "Save Edited Image";
 
-	// saveImage btn event listener
 	saveImage.addEventListener("click", function (evt) {
-		var dt = canvas.toDataURL("image/png");
-		//alert("button clicked");
-		//this.href = dt;
+		var dt = document.getElementById("dropzone").toDataURL("image/png");
 		window.open(dt);
-		//window.open(canvas.toDataURL("image/png"));
 		evt.preventDefault();
 	}, false);
-	document.getElementById("main-content").appendChild(saveImage);
+	// Add button to the page
+	document.getElementById("button-div").appendChild(saveImage);
 
-    // original image button annd event listener
-	var button = document.getElementById('btn-download');
-	button.addEventListener('click', function (e) {
-	    //var dataURL = canvas.toDataURL('image/png');
-	    window.open(originalImage);
-	});
+  	// ################################################################################
+	// Save original image button
+	var saveOriginal = document.createElement("button");
+	saveOriginal.innerHTML = "Save Original Image";
+
+	saveOriginal.addEventListener("click", function(evt) {
+		window.open(originalImage);
+		evt.preventDefault();
+	}, false);
+	// Add button to the page
+	document.getElementById("button-div").appendChild(saveOriginal);
+
+	// ###############################################################################
+	// Brighten the image
+	var brightenImage = document.createElement("button");
+	brightenImage.innerHTML = "Brighten";
+
+	brightenImage.addEventListener("click", function(evt) {
+		pixelManipulate(10, 10, 10);
+	}, false);
+
+	document.getElementById("button-div").appendChild(brightenImage);
+
+	// ################################################################################
+	// Darken the image
+	var darkenImage = document.createElement("button");
+	darkenImage.innerHTML = "Darken";
+
+	darkenImage.addEventListener("click", function(evt) {
+		pixelManipulate(-10, -10, -10);
+	}, false);
+
+	document.getElementById("button-div").appendChild(darkenImage);
+
+	// ################################################################################
+	// Invert the image
+	var invertImage = document.createElement("button");
+	invertImage.innerHTML = ("Invert");
+
+	invertImage.addEventListener("click", function(evt) {
+		invertColors();
+	}, false);
+
+	document.getElementById("button-div").appendChild(invertImage);
+
+	// ################################################################################
+	// Increase red 	
+	var increaseRed = document.createElement("button");
+	increaseRed.innerHTML = "Red";
+
+	increaseRed.addEventListener("click", function(evt) {
+		pixelManipulate(10,0,0);
+	}, false);
+
+	document.getElementById("button-div").appendChild(increaseRed);
+
+	// ################################################################################ // Increase blue var increaseBlue = document.createElement("buttom");
+ 	var increaseGreen = document.createElement("button");
+ 	increaseGreen.innerHTML = "Green";
+
+ 	increaseGreen.addEventListener("click", function(evt){
+ 		pixelManipulate(0,10,0);
+ 	}, false);
+
+ 	document.getElementById("button-div").appendChild(increaseGreen);
+ 	
+ 	// ################################################################################ // Increase blue var increaseBlue = document.createElement("buttom");
+ 	var increaseBlue = document.createElement("button");
+ 	increaseBlue.innerHTML = "Blue";
+
+ 	increaseBlue.addEventListener("click", function(evt) {
+ 		pixelManipulate(0,0,10);
+ 	}, false);
+ 	increaseBlue.value = 0;
+
+ 	document.getElementById("button-div").appendChild(increaseBlue);
+ 	
 }
 
-function addTextBox() {
+
+function addTextBox(id) {
 	//Create an input type dynamically.
 	var element = document.createElement("input");
 
@@ -188,10 +269,16 @@ function drawNote(evt) {
 	ctx.strokeStyle="green";
 	ctx.rect(xPos-12.5,yPos-12.5,25,25);
 	ctx.stroke();
+
+	addTextBox();
 }
 
-function whateverYouWant(xPos, yPos, text){
-
+// Note object to save the notes 
+function Note(xPos, yPos, text){
+	this.id = textEditID++;
+	this.xPos = xPos;
+	this.yPos = yPos;
+	this.text = text;
 }
 
 function invertColors() {
@@ -212,7 +299,7 @@ function invertColors() {
 	ctx.putImageData(imageData, 0, 0);
 }
 
-function darkenImage(r, g, b) {
+function pixelManipulate(r, g, b) {
 	var canvas = document.getElementById("dropzone");
 	var ctx = canvas.getContext("2d");
 
@@ -246,3 +333,5 @@ function brightenImage() {
 	ctx.clearRect(0,0, canvas.width, canvas.height);
 	ctx.putImageData(imageData, 0, 0);
 }
+
+
